518bdbee599a26087cece1a43c62c1139e424ee0
Murat Baki Yücel <bakiyucel38@gmail.com>
2022-02-15 16:14:24 +0300

    Isbank url update Isbank additional optional values set

--- src/Message/AbstractPayment.php
+++ src/Message/AbstractPayment.php
@@ -2,12 +2,14 @@
 
 namespace Omnipay\Nestpay\Message;
 
+use Omnipay\Common\Exception\InvalidCreditCardException;
+
 abstract class AbstractPayment extends AbstractRequest
 {
     protected $transactionType;
 
     protected $endpoints = [
-        'isbank' => 'https://spos.isbank.com.tr/servlet/est3Dgate',
+        'isbank' => 'https://vpos.isbank.co.uk/fim/est3Dgate',
         'akbank' => 'https://www.sanalakpos.com/servlet/est3Dgate',
         'finansbank' => 'https://www.fbwebpos.com/servlet/est3Dgate',
         'halkbank' => 'https://sanalpos.halkbank.com.tr/servlet/est3Dgate',
@@ -43,13 +45,34 @@ abstract class AbstractPayment extends AbstractRequest
         $data['currency'] = $this->getCurrencyNumeric();
         $data['okUrl'] = $this->getReturnUrl();
         $data['failUrl'] = $this->getCancelUrl();
-        $data['storetype'] = '3d_pay';
+        $data['storetype'] = $this->getStoreType();
         $data['rnd'] = time();
         $data['firmaadi'] = $this->getFirmName();
         $data['Faturafirma'] = $this->getBillName();
         $data['tismi'] = $this->getDeliveryName();
         $data['islemtipi'] = $this->transactionType;
         
+        $data["Email"] = $this->getEmail();
+        $data["Fadres"] = $this->getFadres();
+        $data["Fadres2"] = $this->getFadres2();
+        $data["Filce"] = $this->getFilce();
+        $data["Fil"] = $this->getFil();
+        $data["Fpostakodu"] = $this->getFpostakodu();
+        $data["Fulkekodu"] = $this->getFulkekodu();
+        $data["Fulke"] = $this->getFulke();
+        
+        $data["NakliyeFirma"] = $this->getNakliyeFirma();
+        $data["tismi"] = $this->getTismi();
+        $data["tadres"] = $this->getTadres();
+        $data["tadres2"] = $this->getTadres2();
+        $data["tilce"] = $this->getTilce();
+        $data["til"] = $this->getTil();
+        $data["tpostakodu"] = $this->getTpostakodu();
+        $data["tulkekod"] = $this->getTulkekod();
+        $data["tulke"] = $this->getTulke();
+        $data["tel"] = $this->getTel();
+        $data["lang"] = $this->getLang();
+        $data["shipAddrState"] = $this->getShipAddrState();
         $data['taksit'] = null;
         if ($installment = $this->getInstallment()) {
             $data['taksit'] = $installment;
@@ -74,4 +97,171 @@ abstract class AbstractPayment extends AbstractRequest
         return $this->response = new PaymentResponse($this, $data);
     }
 
+    public function getStoreType(){
+        return $this->getParameter('storetype');
+    }
+    
+    public function setStoreType($value){
+    	return $this->setParameter('storetype', $value);
+    }
+
+    public function getEmail(){
+        return $this->getParameter('Email');
+    }
+    
+    public function setEmail($value){
+    	return $this->setParameter('Email', $value);
+    }
+
+    public function getFadres(){
+        return $this->getParameter('Fadres');
+    }
+    
+    public function setFadres($value){
+    	return $this->setParameter('Fadres', $value);
+    }
+
+    public function getFadres2(){
+        return $this->getParameter('Fadres2');
+    }
+    
+    public function setFadres2($value){
+    	return $this->setParameter('Fadres2', $value);
+    }
+
+    public function getFilce(){
+        return $this->getParameter('Filce');
+    }
+    
+    public function setFilce($value){
+    	return $this->setParameter('Filce', $value);
+    }
+
+    public function getFil(){
+        return $this->getParameter('Fil');
+    }
+    
+    public function setFil($value){
+    	return $this->setParameter('Fil', $value);
+    }
+
+    public function getFpostakodu(){
+        return $this->getParameter('Fpostakodu');
+    }
+    
+    public function setFpostakodu($value){
+    	return $this->setParameter('Fpostakodu', $value);
+    }
+
+    public function getFulkekodu(){
+        return $this->getParameter('Fulkekodu');
+    }
+    
+    public function setFulkekodu($value){
+    	return $this->setParameter('Fulkekodu', $value);
+    }
+
+    public function getFulke(){
+        return $this->getParameter('Fulke');
+    }
+    
+    public function setFulke($value){
+    	return $this->setParameter('Fulke', $value);
+    }
+
+    public function getNakliyeFirma(){
+        return $this->getParameter('NakliyeFirma');
+    }
+    
+    public function setNakliyeFirma($value){
+    	return $this->setParameter('NakliyeFirma', $value);
+    }
+
+    public function getTismi(){
+        return $this->getParameter('tismi');
+    }
+    
+    public function setTismi($value){
+    	return $this->setParameter('tismi', $value);
+    }
+
+    public function getTadres(){
+        return $this->getParameter('tadres');
+    }
+    
+    public function setTadres($value){
+    	return $this->setParameter('tadres', $value);
+    }
+
+    public function getTadres2(){
+        return $this->getParameter('tadres2');
+    }
+    
+    public function setTadres2($value){
+    	return $this->setParameter('tadres2', $value);
+    }
+
+    public function getTilce(){
+        return $this->getParameter('tilce');
+    }
+    
+    public function setTilce($value){
+    	return $this->setParameter('tilce', $value);
+    }
+
+    public function getTil(){
+        return $this->getParameter('til');
+    }
+    
+    public function setTil($value){
+    	return $this->setParameter('til', $value);
+    }
+
+    public function getTpostakodu(){
+        return $this->getParameter('tpostakodu');
+    }
+    
+    public function setTpostakodu($value){
+    	return $this->setParameter('tpostakodu', $value);
+    }
+
+    public function getTulkekod(){
+        return $this->getParameter('tulkekod');
+    }
+    
+    public function setTulkekod($value){
+    	return $this->setParameter('tulkekod', $value);
+    }
+
+    public function getTulke(){
+        return $this->getParameter('tulke');
+    }
+    
+    public function setTulke($value){
+    	return $this->setParameter('tulke', $value);
+    }
+
+    public function getTel(){
+        return $this->getParameter('tel');
+    }
+    
+    public function setTel($value){
+    	return $this->setParameter('tel', $value);
+    }
+
+    public function getLang(){
+        return $this->getParameter('lang');
+    }
+    
+    public function setLang($value){
+    	return $this->setParameter('lang', $value);
+    }
+    
+    public function getShipAddrState(){
+        return $this->getParameter('shipAddrState');
+    }
+   
+    public function setShipAddrState($value){
+        return $this->setParameter('shipAddrState', $value);
+    }
 }
